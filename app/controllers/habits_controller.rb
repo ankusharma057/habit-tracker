9ac84746c6a0684e555f079dbd4cd423094ff49a
ankusharma057 <kushsharma057@gmail.com>
2025-07-20 20:22:36 +0530

    Added crud and basic views for habits

---
+++ app/controllers/habits_controller.rb
@@ -0,0 +1,50 @@
+class HabitsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_habit, only: %i[show edit update destroy]
+
+  def index
+    @habits = current_user.habits
+  end
+
+  def show
+  end
+
+  def new
+    @habit = current_user.habits.new
+  end
+
+  def create
+    @habit = current_user.habits.new(habit_params)
+    if @habit.save
+      redirect_to @habit, notice: "Habit created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @habit.update(habit_params)
+      redirect_to @habit, notice: "Habit updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @habit.destroy
+    redirect_to habits_path, notice: "Habit deleted."
+  end
+
+  private
+
+  def set_habit
+    @habit = current_user.habits.find(params[:id])
+  end
+
+  def habit_params
+    params.require(:habit).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-
+  get "habits/index"
+  get "habits/new"
+  get "habits/edit"
+  get "habits/show"
   devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
+  authenticate :user do
+    resources :habits do
+      resources :habit_checkins, only: [:create, :destroy]
+    end
   end
+
+  root to: "habits#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
