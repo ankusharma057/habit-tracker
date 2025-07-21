dd2e2ad24c7f29c477e5ff5adf39f3aa0fa1cbee
ankusharma057 <kushsharma057@gmail.com>
2025-07-21 09:16:20 +0530

    Added consistency calculation

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,45 @@
 class Habit < ApplicationRecord
   belongs_to :user
   has_many :habit_checkins
+
+  def checked_in_dates
+    habit_checkins.pluck(:date).sort
+  end
+
+  def current_streak
+    streak = 0
+    date = Date.today
+
+    while checked_in_dates.include?(date)
+      streak += 1
+      date -= 1
+    end
+    streak
+  end
+
+  def longest_streak
+    streaks = []
+    current_streak = 1
+    sorted = checked_in_dates
+
+    sorted.each_cons(2) do |a, b|
+      if b == a + 1
+        current_streak += 1
+      else
+        streaks << current_streak
+        current_streak = 1
+      end
+    end
+
+    streaks << current_streak
+    streaks.max || 0
+  end
+
+  def consistency_percentage
+    days_since_creation = (Date.today - created_at.to_date).to_i + 1
+    return 0 if days_since_creation <= 0
+
+    (habit_checkins.count.to_f / days_since_creation * 100).round(2)
+  end
+
 end
